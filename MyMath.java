300c6a4b0c6a59bed8ffa13f2de99240eafb92ec
nuhunune <nuhunune@gmail.com>
2019-06-15 14:55:05 +0900

    primefactrizationを追加

--- MyMath.java
+++ MyMath.java
@@ -1,13 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author hydro
- */
 public class MyMath {
-    
+   static String primeFactrization(int input){
+       long firstn=input;
+        long n=firstn;
+        String ans="";
+        for(long i=2;i<=Math.sqrt(n);i++){
+            while(n%i==0){
+                ans+=i+"x";
+                n/=i;
+            }
+        }
+        if(n==1){
+            ans=ans.substring(0,ans.length()-1);
+        }else{
+            ans+=n;
+        }
+        System.out.println(firstn+"="+ans);
+        return ans;
+   } 
 }
