a23ec1b64d012450415d74419cee98df8f9121ba
nuhunune <nuhunune@gmail.com>
2019-06-15 14:56:43 +0900

    インデントを整理した

--- MyMath.java
+++ MyMath.java
@@ -1,21 +1,22 @@
 
 public class MyMath {
-   static String primeFactrization(int input){
-       long firstn=input;
-        long n=firstn;
-        String ans="";
-        for(long i=2;i<=Math.sqrt(n);i++){
-            while(n%i==0){
-                ans+=i+"x";
-                n/=i;
+
+    static String primeFactrization(int input) {
+        long firstn = input;
+        long n = firstn;
+        String ans = "";
+        for (long i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                ans += i + "x";
+                n /= i;
             }
         }
-        if(n==1){
-            ans=ans.substring(0,ans.length()-1);
-        }else{
-            ans+=n;
+        if (n == 1) {
+            ans = ans.substring(0, ans.length() - 1);
+        } else {
+            ans += n;
         }
-        System.out.println(firstn+"="+ans);
+        System.out.println(firstn + "=" + ans);
         return ans;
-   } 
+    }
 }
