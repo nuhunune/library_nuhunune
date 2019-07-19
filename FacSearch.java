50401008151254cf92668d49d501b272f70f2093
nuhunune <48168124+nuhunune@users.noreply.github.com>
2019-07-19 11:21:17 +0900

    とりあえず作ってみた
    アルゴリズムが間違っている

--- FacSearch.java
+++ FacSearch.java
@@ -1,1 +1,33 @@
-
+class FacSearch{
+  int[] indexes;
+  int[] facts;
+  int length;
+  //lengthは10まで!
+  public FacSearch(int length){
+    this.length=length;
+    indexes=new int[length];
+    facts=new int[length+1];
+    facts[0]=1;
+    for(int i=0;i<n;i++){
+      indexes[i]=i;
+      facts[i+1]=facts[i]*(i+1);
+    }
+    
+  }
+  void update(){
+    for(int i=0;i<length-1;i++){
+      if(indexes[i]>indexes[i+1]){
+        swap(i);
+        break;
+      }else{
+        swap(i);
+      }
+    }
+  }
+  void swap(int index){
+    int tmp;
+    tmp=indexes[index];
+    indexes[index]=indexes[index+1];
+    indexes[index+1]=tmp;
+  }
+}
